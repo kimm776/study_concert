3303b8053d397b469cf4fbe98388f494a8c4a9f3
kimm776 <88071886+kimm776@users.noreply.github.com>
2024-10-18 04:21:38 +0900

    기타 설정

--- src/main/java/com/hhplus/concert/ConcertApplication.java
+++ src/main/java/com/hhplus/concert/ConcertApplication.java
@@ -1,8 +1,10 @@
-package com.hhplus.app.concert;
+package com.hhplus.concert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ConcertApplication {
 
